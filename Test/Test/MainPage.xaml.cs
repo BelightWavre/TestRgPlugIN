c0e00fd2479d17391a38c47784ec2fefe06aff61
Rus.Wavre <Rus.Wavre@gmail.com>
2021-06-20 09:59:12 +0200

    Add project files.

---
+++ Test/Test/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Test
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            Rg.Plugins.Popup.Services.PopupNavigation.Instance.PushAsync(new MyPopup());
+
+        }
+    }
+}
